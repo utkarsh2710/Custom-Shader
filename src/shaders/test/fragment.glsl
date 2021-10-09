a29286c680dc49f4abd4fa1383dce746663be99e
Utkarsh Pandey <utkarsh.pandey2710@gmail.com>
2021-10-09 17:25:25 +0530

    Basic Flag

---
+++ src/shaders/test/fragment.glsl
@@ -0,0 +1,14 @@
+uniform vec3 uColor; 
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+//varying float vRandom;
+
+void main()
+{
+    vec4 textureColor = texture2D(uTexture, vUv); 
+    textureColor.rgb *=vElevation*2.0+0.6; 
+    gl_FragColor = textureColor;
+
+} 
